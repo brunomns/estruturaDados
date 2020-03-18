f06569d1d93840e5aa0efa0e46cc56fb6123c037
Bruno Miguel <49412285+brunomns@users.noreply.github.com>
2020-03-18 16:31:11 -0300

    Update VetorExemplo.java

--- codigos/0002_VetorExemplo/src/vetorexemplo/VetorExemplo.java
+++ codigos/0002_VetorExemplo/src/vetorexemplo/VetorExemplo.java
@@ -5,6 +5,8 @@
  */
 package vetorexemplo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bmnso
@@ -16,15 +18,33 @@ public class VetorExemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Aluno vetAl[] = new Aluno[5];
+        Aluno vetAl[] = new Aluno[3];
+        // instancia o Scanner para possibilitar a leitura via teclado...
+        Scanner tec = new Scanner(System.in);
         
         //Cria os objetos do tipo aluno dentro do vetor...
         for (int i = 0; i < vetAl.length; i++) {
-           vetAl[i] = new Aluno("nomeAluno"+i,"mail"+i+"@uenp.edu",Integer.toString(i));            
+           
+            String nome;
+            String email;
+            String matricula;
+           
+            System.out.println("Digite o nome do aluno "+ i);
+            nome = tec.nextLine();
+
+            System.out.println("Digite o e-mail do aluno "+ i);
+            email = tec.nextLine();
+
+            System.out.println("Digite a matricula do aluno "+ i);
+            matricula = tec.nextLine();
+                       
+            vetAl[i] = new Aluno(nome, email, matricula);            
         }
+        
+        //vetAl[2].nome = "Maria";
         //Imprime o nome dos alunos 
         for (int i = 0; i < vetAl.length; i++) {
-            System.out.println("Aluno: "+vetAl[i].nome);
+            System.out.println("Aluno: "+vetAl[i].nome + " ");
             
         }
         
